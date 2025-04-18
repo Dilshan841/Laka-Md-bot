be88263da138f036dd0a4d25e59a440ad6813435
Dilshan841 <rathnayakehemantha427@gmail.com>
2025-04-18 11:06:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=RpJEUQ6C#oi-XZ5n3reYoqnIkq0NTDtGH4X_xOM7zhiPHmioI714",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=99ZmmTyJ#iNoWqVM2xaLM6Etp4XxM-MmJ-vznPN85_r8C3j-7FEk",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
